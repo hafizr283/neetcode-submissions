3f494e67e8c79b98cebac46e2bac234b44edd07a
Md. Hafizur Rahman <hafizursiam@gmail.com>
2026-07-31 20:34:07 +0600

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int idx= lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(idx!=nums.size() and nums[idx]==target) return idx;
+        return -1;
+
+    }
+};
