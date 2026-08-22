52ef78de48439485a8b2cea6f212ce7666ca50eb
Md. Hafizur Rahman <hafizursiam@gmail.com>
2026-08-22 21:05:01 +0600

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+           
+        return ways(n,dp);
+    }
+    public int ways(int n,int []dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=ways(n-1,dp)+ways(n-2,dp);
+
+
+    }
+}
